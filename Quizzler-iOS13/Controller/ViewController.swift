3eeb0353f68a230b899abc45cd80f4838733306f
Zackry Langford <zackrylangford@gmail.com>
2023-11-14 10:29:05 -0500

    created a multiple choice option

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -13,11 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var option1: UIButton!
+    
+    @IBOutlet weak var option2: UIButton!
+    
+    
+    @IBOutlet weak var option3: UIButton!
     
     var quizBrain = QuizBrain()
-
+    
     
     
     override func viewDidLoad() {
@@ -37,22 +42,26 @@ class ViewController: UIViewController {
             sender.backgroundColor = UIColor.red
         }
         quizBrain.nextQuestion()
-
+        
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
-    
-    
-    
-}
-
+        
+        
+        
+    }
     
     
     @objc func updateUI() {
         questionLabel.text = quizBrain.getQuestionText()
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
+        option1.setTitle(quizBrain.getAnswerText(at: 0), for: .normal)
+        option2.setTitle(quizBrain.getAnswerText(at: 1), for: .normal)
+        option3.setTitle(quizBrain.getAnswerText(at: 2), for: .normal)
+        option1.backgroundColor = UIColor.clear
+        option2.backgroundColor = UIColor.clear
+        option3.backgroundColor = UIColor.clear
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
     }
-}
 
+    
+}
