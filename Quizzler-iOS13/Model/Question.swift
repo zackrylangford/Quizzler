fd97d1b5d51aba5cca90e214f6bed01449055d00
Zackry Langford <zackrylangford@gmail.com>
2023-11-14 10:01:56 -0500

    completed initial project, not multiple choice

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Zackry Langford on 11/13/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
